d9fc08b4d2b65db8ec9a9c0152adabe89ee40648
Bharath <bharath.sudarsanam04@gmail.com>
2023-02-23 12:44:10 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,8 +16,9 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
                 connection.login(user = my_email, password=my_pass)
                 connection.sendmail(from_addr=my_email, to_addrs="sendermail", msg=f"Subject: Hey "
                                                                                                        f"there\n\nMy "
-                                                                                                       f"name is "
+                                                                                                       f"name is"
                                                                                                        f"your name "
+                                                                                                       f"Happy Birthday🎂"
                                                                                                        f"\n\n"
                                                                                                        f"{random_quote}")
                 print("Message sent")
